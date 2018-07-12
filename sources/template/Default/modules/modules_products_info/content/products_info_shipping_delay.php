e54f5107822f2f71c3804dd21f5f976134468aed
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-07-12 08:53:03 -0400

    update json

---
+++ sources/template/Default/modules/modules_products_info/content/products_info_shipping_delay.php
@@ -0,0 +1,8 @@
+<div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
+  <div class="separator"></div>
+  <div style="float: <?php echo MODULE_PRODUCTS_INFO_SHIPPING_DELAY_POSITION; ?>">
+    <div class="modulesProductsInfoShippingDelay">
+      <span class="modulesProductsInfoShippingDelay"><h3><?php echo $products_shipping_delay; ?></h3></span>
+    </div>
+  </div>
+</div>
